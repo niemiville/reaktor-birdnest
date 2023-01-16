e15612100c9d3e5abb07ecdbc62bf4743d5e223b
Ville Niemi <vilsuxn@gmail.com>
2023-01-16 19:04:56 +0200

    added styles for frontend and removed react logos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,41 @@
-
 import React, { useState, useEffect } from 'react'
-import './App.css';
+import './birdnestStyles.css'
 import { getViolations } from './services/services';
 
 function App() {
   const [violations, setViolations] = useState([])
-
+  const DATA_REFRESH_INTERVAL = 7500;
+  
   useEffect(() => {
     const interval = setInterval(() => {
       getViolations().then(violations =>
         setViolations(violations)
       )  
-    }, 5000);
+    }, DATA_REFRESH_INTERVAL);
     return () => clearInterval(interval);
   }, [])
 
   return (
     <div>
-      <h1>Birdnest</h1>
-      <ul>
-        {violations.map((v) => 
-          <li key={v.serialNumber}>{v.firstName} {v.lastName} {v.lastSeen} {v.closestDistanceToNest} {v.phoneNumber} {v.email}</li>
-        )}
-      </ul>
+      <h1>Birdnest No Drone Zone Violators</h1>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Last seen</th>
+          <th>Closest distance to the nest</th>
+          <th>Phone</th>
+          <th>Email</th>
+        </tr>
+          {violations.map((v) => 
+            <tr key={v.serialNumber}>
+              <td>{v.firstName} {v.lastName}</td> 
+              <td>{v.lastSeen}</td>
+              <td>{(parseInt(v.closestDistanceToNest, 10)/1000).toFixed(2)} meters</td>
+              <td>{v.phoneNumber}</td>
+              <td>{v.email}</td>
+            </tr>
+          )}
+      </table>
     </div>
   );
 }
