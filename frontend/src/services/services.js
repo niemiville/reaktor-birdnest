5d5a007f6a747b45fad52fd1f52780bf92382305
Ville Niemi <vilsuxn@gmail.com>
2023-01-17 18:26:02 +0200

    updated link

--- frontend/src/services/services.js
+++ frontend/src/services/services.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const violationsUrl = 'https://birdnest-api-vn.onrender.com/api/violations'
+const violationsUrl = 'https://birdnest-api-vn.onrender.com/api/violations/'
 
 const getViolations = async () => {
   const violations = await axios.get(violationsUrl);
